4d67ef60487685c76a17bf40a0663675e56edc50
ryanabar <21rabar@athenian.org>
2020-09-24 16:51:00 +0000

    FINAL

--- my_code.py
+++ my_code.py
@@ -1,18 +1,30 @@
 # Collaborators (including web sites where you got help: (enter none if you didn't need help)
-#
+# Megan during class time
+
 
 def avg(user_list):
     sumOfNumbers = 0
     for x in user_list:
-        sumOfNumbers = sumOfNumbers + x          
+        sumOfNumbers = sumOfNumbers + x
     avg = sumOfNumbers / len(user_list)
     return avg
 
-user_list = [9,8,2,1,44]
-print("The average is", avg(user_list))
+
+if __name__ == '__main__':
+
+    user_list = []
+    num = 0
+    while num != 'done':
+        num = input("Enter a number to be averaged. If you are done, type 'done'. ")
+        if num == 'done':
+            break
+        num = int(num)
+        user_list.append(num)
 
 
-#if __name__ == '__main__':
+    print("The average of these values is ", avg(user_list))
+
+
 
     # test your fucntion with this print statement before writing the input loop
     
